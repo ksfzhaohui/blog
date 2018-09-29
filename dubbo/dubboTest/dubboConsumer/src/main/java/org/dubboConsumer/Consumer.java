97dea760fd6fab8bf2c316fb45dd1f806522f623
zhaohui <ksfzhaohui@126.com>
2018-09-29 18:10:37 +0800

    add

--- dubbo/dubboTest/dubboConsumer/src/main/java/org/dubboConsumer/Consumer.java
+++ dubbo/dubboTest/dubboConsumer/src/main/java/org/dubboConsumer/Consumer.java
@@ -7,19 +7,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.dubboApi.DemoService;
+import com.dubboApi.bean.TestBean;
 
 public class Consumer {
 
-	public static void main(String[] args) throws InterruptedException,
-			ExecutionException {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"dubbo-consumer.xml");
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
 		context.start();
 		DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
 
-		System.out.println(demoService.syncSayHello("world"));
-		System.out.println(demoService.asyncSayHello("world"));
-		Future<String> futrue = RpcContext.getContext().getFuture();
-		System.out.println(futrue.get());
+//		System.out.println(demoService.syncSayHello("world"));
+//		System.out.println(demoService.asyncSayHello("world"));
+//		Future<String> futrue = RpcContext.getContext().getFuture();
+//		System.out.println(futrue.get());
+
+		System.out.println(demoService.sayHello(new TestBean("zhaohui", 99, "nanjing")));
 	}
 }
--- dubbo/dubboTest/dubboProvider/src/main/java/com/dubboProvider/DemoServiceImpl.java
+++ dubbo/dubboTest/dubboProvider/src/main/java/com/dubboProvider/DemoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dubboProvider;
 
 import com.dubboApi.DemoService;
+import com.dubboApi.bean.TestBean;
 
 public class DemoServiceImpl implements DemoService {
 
@@ -12,5 +13,11 @@ public class DemoServiceImpl implements DemoService {
 	@Override
 	public String asyncSayHello(String name) {
 		return "async Hello " + name;
+	}
+
+	@Override
+	public String sayHello(TestBean bean) {
+		System.out.println("sayHello====="+bean.getName());
+		return bean.getName();
 	}
 }
