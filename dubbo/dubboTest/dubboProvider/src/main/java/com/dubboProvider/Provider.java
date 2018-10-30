e1db446415cd842f0e562058a6b6645d4ada181b
zhaohui <ksfzhaohui@126.com>
2018-10-30 15:23:59 +0800

    日志

--- dubbo/dubboTest/dubboProvider/src/main/java/com/dubboProvider/Provider.java
+++ dubbo/dubboTest/dubboProvider/src/main/java/com/dubboProvider/Provider.java
@@ -1,14 +1,18 @@
 package com.dubboProvider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Provider {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Provider.class);
 
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "dubbo-provider.xml" });
 		context.start();
-		
+		LOGGER.info("server starting...");
 		System.in.read(); // 按任意键退出
 	}
 }
