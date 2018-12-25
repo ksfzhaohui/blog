6c73f87ec8c6793279282c273d2aa9f65852b178
zhaohui <ksfzhaohui@126.com>
2018-12-25 10:17:22 +0800

    spi

---
+++ SPI/src/main/java/com/spi/App.java
@@ -0,0 +1,40 @@
+package com.spi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		String url = "jdbc:mysql://localhost:3306/db3";
+		String username = "root";
+		String password = "root";
+		String sql = "update travelrecord set name=\'bbb\' where id=1";
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			con.setAutoCommit(false);
+			pstmt.execute();
+			con.commit();
+			System.out.println("update success");
+		} catch (Exception se) {
+			if (con != null) {
+				try {
+					System.out.println("rollback");
+					con.rollback();
+				} catch (SQLException e) {
+					System.err.println("rollback error");
+				}
+			}
+			se.printStackTrace();
+		}
+	}
+}
