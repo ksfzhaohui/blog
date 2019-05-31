e4b61797c2ab198bd1a65f8b788a69fa11cfbbf5
zhaohui <ksfzhaohui@126.com>
2019-05-31 18:11:42 +0800

    sql

--- mybatis/mybatis/mybatis.sql
+++ mybatis/mybatis/mybatis.sql
@@ -4,7 +4,7 @@ Source Host     : localhost:3306
 Source Database : mybatis
 Target Host     : localhost:3306
 Target Database : mybatis
-Date: 2019-05-30 17:03:00
+Date: 2019-05-31 18:11:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,7 +22,61 @@ CREATE TABLE `blog` (
 -- ----------------------------
 -- Records of blog
 -- ----------------------------
-INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'java', '101');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '101');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '102');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '103');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '104');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '105');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '106');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '107');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '108');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '109');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '110');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '111');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '112');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '113');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '114');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '115');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '116');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '117');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '118');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '119');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '120');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '121');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '122');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '123');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '124');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '125');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '126');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '127');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '128');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '129');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '130');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '131');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '132');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '133');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '134');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '135');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '136');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '137');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '138');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '139');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '140');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '141');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '142');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '143');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '144');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '145');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '146');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '147');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '148');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '149');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '150');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '151');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '152');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '153');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '154');
+INSERT INTO `blog` VALUES ('java is hello world', 'ksfzhaohui', 'new_java', '155');
 
 -- ----------------------------
 -- Table structure for role
@@ -46,6 +100,7 @@ INSERT INTO `role` VALUES ('world', 'kitty', '116');
 INSERT INTO `role` VALUES ('world', 'kitty', '117');
 INSERT INTO `role` VALUES ('world', 'kitty', '118');
 INSERT INTO `role` VALUES ('world', 'kitty', '119');
+INSERT INTO `role` VALUES ('world', 'kitty', '120');
 
 -- ----------------------------
 -- Table structure for student
