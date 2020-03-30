c2b12492814ae94184c5eb5afae35ce4d82ab1d8
zhaohui <ksfzhaohui@126.com>
2020-03-30 23:26:22 +0800

    spring

--- spring/src/main/java/com/spring/Person.java
+++ spring/src/main/java/com/spring/Person.java
@@ -3,13 +3,15 @@ package com.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.spring.bean.PersonBean;
+
 public class Person {
 
 	public static void main(String[] args) {
 		System.out.println("开始初始化容器");
 		ApplicationContext ac = new ClassPathXmlApplicationContext("person.xml");
 		System.out.println("xml加载完毕");
-		Person person1 = (Person) ac.getBean("person1");
+		PersonBean person1 = (PersonBean) ac.getBean("person1");
 		System.out.println(person1);
 		System.out.println("关闭容器");
 		((ClassPathXmlApplicationContext) ac).close();
