e5ec48f641d4d895e32164c86d3187f400d61659
zhaohui <ksfzhaohui@126.com>
2020-03-31 22:58:12 +0800

    spring

--- spring/src/main/java/com/spring/processor/MyBeanPostProcessor.java
+++ spring/src/main/java/com/spring/processor/MyBeanPostProcessor.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class MyBeanPostProcessor implements BeanPostProcessor {
 
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("postProcessBeforeInitialization被调用");
+		System.out.println("postProcessBeforeInitialization被调用，beanName="+beanName);
 		return bean;
 	}
 
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("postProcessAfterInitialization被调用");
+		System.out.println("postProcessAfterInitialization被调用，beanName="+beanName);
 		return bean;
 	}
 }
