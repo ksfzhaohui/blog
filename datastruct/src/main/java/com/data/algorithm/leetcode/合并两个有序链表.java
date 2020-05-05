579c0add99a8c53bc63b482591fce20c6e9f74bb
zhaohui <ksfzhaohui@126.com>
2020-05-05 23:09:08 +0800

    力扣

--- datastruct/src/main/java/com/data/algorithm/leetcode/合并两个有序链表.java
+++ datastruct/src/main/java/com/data/algorithm/leetcode/合并两个有序链表.java
@@ -31,11 +31,13 @@ public class 合并两个有序链表 {
 		while (l1 != null) {
 			ListNode ln = new ListNode(l1.val);
 			first.next = ln;
+			first = ln;
 			l1 = l1.next;
 		}
 		while (l2 != null) {
 			ListNode ln = new ListNode(l2.val);
 			first.next = ln;
+			first = ln;
 			l2 = l2.next;
 		}
 
@@ -43,17 +45,17 @@ public class 合并两个有序链表 {
 	}
 
 	public static void main(String[] args) {
-		ListNode l11 = new ListNode(1);
-		ListNode l12 = new ListNode(2);
-		ListNode l13 = new ListNode(4);
+		ListNode l11 = new ListNode(-9);
+		ListNode l12 = new ListNode(3);
+//		ListNode l13 = new ListNode(4);
 		l11.next = l12;
-		l12.next = l13;
+//		l12.next = l13;
 
-		ListNode l21 = new ListNode(1);
-		ListNode l22 = new ListNode(3);
-		ListNode l23 = new ListNode(4);
+		ListNode l21 = new ListNode(5);
+		ListNode l22 = new ListNode(7);
+//		ListNode l23 = new ListNode(4);
 		l21.next = l22;
-		l22.next = l23;
+//		l22.next = l23;
 
 		System.out.println(print(l11));
 		System.out.println(print(l21));
